44662d67e2bc1ce72c7fafc80c4b8efb1714fcc4
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-04-23 09:01:04 -0600

    Feature: Student, User and UserPrincipal entities

---
+++ src/main/java/com/springjwtexample/springjwtexample/Entities/User.java
@@ -0,0 +1,25 @@
+package com.springjwtexample.springjwtexample.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+    
+    private String username;
+    private String passwrd  ;
+}
