8a9bc5a3597af10aafcbe8deec419035830766c8
Geo290 <jorgeemmanuel.2003@gmail.com>
2026-04-23 09:03:32 -0600

    Feature: Necesary resources (MVN settings, Entry Point, etc)

---
+++ src/main/java/com/springjwtexample/springjwtexample/SpringjwtexampleApplication.java
@@ -0,0 +1,13 @@
+package com.springjwtexample.springjwtexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjwtexampleApplication.class, args);
+	}
+
+}
